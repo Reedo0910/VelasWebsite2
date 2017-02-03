71a91a708d0dcb5d87af5fd436e9cad526072091
Reedo0910 <sea.sand@live.cn>
2017-02-03 22:48:13 +0800

    2017-02-03 22:48:09

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
 gulp.task('sass',function(){
     return gulp.src('public/scss/**/*.+(sass|scss)')
     .pipe(sass())
     .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.reload({
+        stream:true
+    }))
 })
 
-gulp.task('watch',function(){
+gulp.task('browserSync',function(){
+    browserSync({
+        server:{
+            baseDir:'public'
+        },
+    })
+})
+
+gulp.task('watch',['browserSync','sass'],function(){
     gulp.watch('public/scss/**/*.+(sass|scss)',['sass']);
+    gulp.watch('public/js/**/*.js',browserSync.reload);
+    gulp.watch('public/html/**/*.html',browserSync.reload);
 })
 
-gulp.task('default',['watch']);
+gulp.task('default',['browserSync','sass','watch']);
