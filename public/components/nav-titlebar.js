df3969979e540143f2ed0881596002147490bc66
Reedo0910 <sea.sand@live.cn>
2017-02-05 14:55:58 +0800

    时间

--- public/components/nav-titlebar.js
+++ public/components/nav-titlebar.js
@@ -4,13 +4,13 @@ var navTitle = {
       <li><a href="index.html">Home</a></li>\
       <li><a href="peace.html">Peace</a></li>\
       <li><a href="log.html">Log</a></li>\
-      <li class="nav-talk-button"><a href="http://blog.velas.xyz/">Velas Talk</a></li>\
+      <li class="nav-talk-button"><a href="http://blog.velas.xyz/">Talk</a></li>\
       </ul>'
 }
 
 function setActive(index) {
     var liList = document.getElementById('navTitle').getElementsByTagName('li');
-    if (index < liList.length) {
+    if (index < liList.length - 1) {
         liList[index].className = 'active';
-    }
+    } 
 }
