d04d22cee62ea34ca9c5dcb429a62f8c7539b060
Reedo0910 <sea.sand@live.cn>
2017-02-05 17:01:15 +0800

    2017-02-05 17:01:12

---
+++ public/components/footer.js
@@ -0,0 +1,18 @@
+var footer = {
+    template: '\
+    <div id="footer">\
+        <p class="footer-contact">如何联系Zeee?</p>\
+        <div class="hr"></div>\
+        <ul class="share-group">\
+            <a href="mailto:sea.sand@live.cn"><li class="fa fa-envelope fa-lg"></li></a>\
+            <a href="http://weibo.com/reedo"><li class="fa fa-weibo fa-lg"></li></a>\
+            <a href="https://github.com/Reedo0910"><li class="fa fa-github fa-lg"></li></a>\
+        </ul>\
+        <div id="copy">\
+            <div class="gototop">\
+                <a href="#" class="fa fa-angle-up fa-2x"></a>\
+            </div>\
+            <p>Zeee@Velas 2017</p>\
+        </div>\
+    </div>'
+}
--- public/components/nav-titlebar.js
+++ public/components/nav-titlebar.js
@@ -1,16 +1,35 @@
 var navTitle = {
     template: '\
+    <nav id="nav">\
+      <div id="logo">\
+      <a href="index.html">Velas</a>\
+      </div>\
       <ul id="navTitle">\
       <li><a href="index.html">Home</a></li>\
       <li><a href="peace.html">Peace</a></li>\
       <li><a href="log.html">Log</a></li>\
       <li class="nav-talk-button"><a href="http://blog.velas.xyz/">Talk</a></li>\
-      </ul>'
+      </ul>\
+      </nav>'
 }
 
 function setActive(index) {
     var liList = document.getElementById('navTitle').getElementsByTagName('li');
     if (index < liList.length - 1) {
         liList[index].className = 'active';
-    } 
+    }
+}
+
+function scrollOnTop() {
+    var top = document.documentElement.scrollTop || document.body.scrollTop;
+    var navBar = document.getElementById('nav');
+    if (top <= 50) {
+        navBar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+        navBar.style.boxShadow = 'none';
+        navBar.style.paddingTop = '20px';
+    } else {
+        navBar.style.backgroundColor = 'rgba(92, 107, 192, 0.59)';
+        navBar.style.boxShadow = '#444 0 0 10px';
+        navBar.style.paddingTop = '10px';
+    }
 }
