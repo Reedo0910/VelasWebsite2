4e6c1866f2d88b4e5c7a7480e55b3a096dc128a4
Reedo0910 <sea.sand@live.cn>
2017-03-21 01:27:02 +0800

    2017-03-21 01:25:54

---
+++ public/js/music.json.js
@@ -0,0 +1,20 @@
+var musicData = [{
+    Artist: 'Paramore',
+    ArtistPic: '../images/album/artist/paramore.jpg',
+    BackgroundColor: 'rgba(90, 90, 90, 0.7)',
+    Albums: [{
+        albumPic: './images/album/riot!.jpg',
+        albumName: 'Riot!',
+        songTitle: 'Misery Business',
+        albumYear: 2007,
+        themeColor: 'rgba(233, 88, 64, 0.5)',
+        albumIntro: '<span>“一出现便是巅峰”，<em>Riot!</em>对Paramore来说便是这样的一张专辑。</span><span>如封面那些躁动的Riot!一样，整张专辑都充斥着青春的不安分与癫狂。与此同时，歌曲的完成度和主唱Hayley对朋克风格的驾驭程度之高都让人吃惊，将一专<em>All We Know Is Falling</em>时的青涩一洗而空，其中的不少曲目也成了Paramore的代表作。</span>'
+    }, {
+        albumPic: './images/album/brand_new_eyes.jpg',
+        albumName: 'Brand New Eyes',
+        songTitle: 'The Only Exception',
+        albumYear: 2009,
+        themeColor: 'rgba(229, 219, 209, 0.5)',
+        albumIntro: '<span>当Paramore来到第三张专辑的时候，轻狂似乎褪去了些，显得更为成熟了。</span><span>在<em>Brand New Eyes</em>中，不少歌曲多了对爱情与人生的思考，反而少了二专时喊着“Crush Crush Crush”那种不可一世的叛逆。让人不禁感叹：“原来Paramore唱慢歌的时候也那么动人。”</span><span>我个人认为，正是因为三专对不同的风格的尝试与融合，使得它成为了Paramore最具代表性的一张专辑。</span>'
+    }]
+}]
